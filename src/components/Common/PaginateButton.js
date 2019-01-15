864ba86b48a2e65192b6a5a10f058af86957baa4
Lemuel <lemuel@Hans-Air.fios-router.home>
2019-01-15 04:15:09 -0500

    cleanup paginatebutton

--- src/components/Common/PaginateButton.js
+++ src/components/Common/PaginateButton.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import Fab from '@material-ui/core/Fab';
 import NavigationIcon from '@material-ui/icons/Navigation';
-import { withStyles } from '@material-ui/core/styles';
-
-// const styles = theme => ({
-//     fab: {
-//       margin: theme.spacing.unit,
-//     },
-//     extendedIcon: {
-//       marginRight: theme.spacing.unit,
-//     },
-//   });
 
 const PaginateButton = props => {
-    const { searchQuery, fetchNextPage, pagination, classes } = props;
+    const { searchQuery, fetchNextPage, pagination } = props;
     const { count, total_count, offset } = pagination;
     const paginateCTA = total_count - count > count ? `Load ${count} more` : "Load all";
-    console.log(total_count, 'totalCount')
-    console.log(count, 'count')
-
     const handlePaginate = () => {
         let newOffset = offset + count
         fetchNextPage(searchQuery, count, newOffset);
